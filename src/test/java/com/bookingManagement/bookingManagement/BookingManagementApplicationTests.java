9605afdef5ce25097f85d2b46ed7831ae3561e2e
nikocostantino <55782244+nikocostantino@users.noreply.github.com>
2024-05-03 10:06:34 +0100

    Initial commit

---
+++ src/test/java/com/bookingManagement/bookingManagement/BookingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingManagement.bookingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
