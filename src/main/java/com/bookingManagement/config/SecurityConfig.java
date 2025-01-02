b04daa6d8fe73804a7afccacb42ab61110ea3d19
nikocostantino <nicoc840@gmail.com>
2025-01-02 16:46:21 +0100

    prototype done

--- src/main/java/com/bookingManagement/config/SecurityConfig.java
+++ src/main/java/com/bookingManagement/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
